c5d9114490cea32992f88825d194eab7c0ec2440
QuarK264 <alexey.danilov@hotmail.com>
2014-11-20 23:42:19 +0300

    2 курс, Шаблонное программирование, Косольный ввод чисел

---
+++ DAA.DP.InputControl/Input.cs
@@ -0,0 +1,82 @@
+﻿namespace DAA.DP.InputControl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class Input
+    {
+        internal static int Int32(string phrase)
+        {
+            Console.WriteLine(phrase);
+            ConsoleKeyInfo keyInfo;
+            StringBuilder buffer = new StringBuilder(20);
+            IList<char> validChars = GetValidChars().ToList();
+            while (true)
+            {
+                keyInfo = Console.ReadKey(true);
+                if (validChars.Contains(keyInfo.KeyChar))
+                {
+                    buffer.Append(keyInfo.KeyChar);
+                    ClearConsoleLine();
+                    Console.Write(buffer.ToString());
+                    continue;
+                }
+                if (keyInfo.Key == ConsoleKey.Backspace)
+                {
+                    if (buffer.RemoveLastSymbol())
+                    {                        
+                        Console.Write(buffer.ToString());
+                        continue;
+                    }
+                }
+                if (keyInfo.Key == ConsoleKey.Enter && buffer.IsNotEmpty())
+                {
+                    break;
+                }
+                Console.Beep();
+            }
+            Console.WriteLine();
+
+            return int.Parse(buffer.ToString());
+        }
+
+        private static void ClearConsoleLine()
+        {
+            var str = new string('\b', Console.CursorLeft);
+            Console.Write(str);
+        }
+
+        public static bool IsEmpty(this StringBuilder StrBuilder)
+        {
+            return StrBuilder.Length == 0;
+        }
+
+        public static bool IsNotEmpty(this StringBuilder StrBuilder)
+        {
+            return !StrBuilder.IsEmpty();
+        }
+
+        public static bool RemoveLastSymbol(this StringBuilder StrBuilder)
+        {
+            if (StrBuilder.IsNotEmpty())
+            {
+                StrBuilder.Remove(StrBuilder.Length - 1, 1);
+                Console.Clear();
+                return true;
+            }
+
+            return false;
+        }
+
+        private static IEnumerable<char> GetValidChars()
+        {
+            foreach (var item in "0123456789")
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ DAA.DP.InputControl/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DAA.DP.InputControl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main()
+        {
+            int value = Input.Int32("Введите число");
+            Console.WriteLine("Вы ввели:");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
